cda7fe9e243817d8a5f28d3bf02040d78fe2d977
T-250 <122810819+T-250@users.noreply.github.com>
2023-03-17 14:25:52 -0700

    first c language program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,9 +3,8 @@
  * main-entry point
  * return: always 0 (success)
  */
-int main(void)
+int main()
 {
-	puts("\"Programming is like building a multilingual puzzle);
-
-	return (0);
+    puts("\"Programming is like building a multilingual puzzle");
+    return 0;
 }
