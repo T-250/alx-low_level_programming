3f357ca1cbb84442d9ca9c70f75a29e59fe22271
T-250 <122810819+T-250@users.noreply.github.com>
2023-03-20 18:35:14 -0700

    No more q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char m;
+
+	for (m = 'a'; m <= 'z'; m++)
+		if (m != q && m != e)
+			putchar(m);
+	putchar('\n');
+	return (0);
+}
