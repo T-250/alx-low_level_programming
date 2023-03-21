6e2bca21a1e756c233f3a48b5b290cb54a48ddcc
T-250 <122810819+T-250@users.noreply.github.com>
2023-03-20 18:46:05 -0700

    Decimal numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+* main - Entry point
+* Return: Always 0 (success)
+*/
+int main(void)
+{
+	char ch = '0';
+
+	for (ch = '0'; ch <= '9'; ch++)
+		putchar(ch);
+	putchar('\n');
+	return (0);
+}
