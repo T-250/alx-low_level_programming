044ea35a5868a361912522bedf56790d5fe2c4c1
T-250 <122810819+T-250@users.noreply.github.com>
2023-03-25 04:01:56 -0700

    task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,9 +1,14 @@
 #include "main.h"
+/**
+ * print_alphabet - prints alphabets
+ * Return: always 0
+ */
 
 void print_alphabet(void)
 {
 	char j;
+
 	for (j = 'a'; j <= 'z'; j++)
-		_putchar('j');
+		_putchar(j);
 	_putchar('\n');
 }
