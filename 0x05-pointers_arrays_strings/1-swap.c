aab263e31bc16b0a1f33bd21184b3648cebb7f4d
T-250 <122810819+T-250@users.noreply.github.com>
2023-03-29 21:29:46 -0700

    Task 1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * swap_int - swaps the value of a and b
+ * @a: an integer to swap with b
+ * @b: an integer to swap with a
+ * Return: Always 0
+ */
+
+void swap_int(int *a, int *b)
+{
+	*a = b;
+	*b = a;
+}
