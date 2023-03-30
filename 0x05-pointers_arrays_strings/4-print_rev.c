d3bd045995ce724116173ed1c81d1aca4900093e
T-250 <122810819+T-250@users.noreply.github.com>
2023-03-30 16:58:07 -0700

    task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_rev - reverse print strings
+ * @s: string to be reversed printed
+ * Return: always 0
+ */
+
+void print_rev(char *s)
+{
+	/*first step is to count the number of strings*/
+	int i;
+	int count = 0;
+
+	for (j = 0; s[j] != '\0'; j++) /* counts string xters*/
+		count++;
+	for (j = count; s[j] >= 0; j--) /*using the count from above*/
+		_putchar(s[j]); /* to reverse print the string*/
+	_putchar('\n');
+}
