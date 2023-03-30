048ab57b74d405239949181b248080f05c56e690
T-250 <122810819+T-250@users.noreply.github.com>
2023-03-30 16:04:06 -0700

    task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strlen - counts number of strings
+ * @s: string to count
+ * Return: int (count)
+ */
+
+int _strlen(char *s)
+{
+	int i;
+	int count;
+
+	for (i = 0; *s[i] != '\0'; i++)
+	{
+		count++;
+	}
+	return (count);
+}
